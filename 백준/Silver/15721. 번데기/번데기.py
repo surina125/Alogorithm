7b75d749cd45919909828c04ecd9e1e0f14f7d68
surina125 <jhyunsoo1125@gmail.com>
2024-06-07 00:40:10 +0900

    [Silver V] Title: 번데기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15721. 번데기/번데기.py
+++ 백준/Silver/15721. 번데기/번데기.py
@@ -13,15 +13,19 @@ for i in range(2, 10001):
     len_sen = len(sen)
 
     for j in range(len_sen):
+        # 원 모양으로 사람들이 둥글게 앉았기 때문에 한바뀌 돌면 번호 리셋
         if per == persons:
             per = 0
 
         if sen[j] == tar:
             t += 1
 
+        # 종료 조건
         if t == tar_num:
             flag = 1
             break
+        
+        # 다음차례 사람
         per += 1
 
 print(per)
