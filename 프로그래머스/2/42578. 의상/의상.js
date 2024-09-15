77f1d21ac74be633db5e64516a6e9a7f709c3a73
surina125 <jhyunsoo1125@gmail.com>
2024-09-16 08:04:45 +0900

    [level 2] Title: 의상, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -10,12 +10,12 @@ function solution(clothes) {
     const len_types = types.length
     let new_clothes = Array.from({ length: len_types }, () => [])
     
-    for (i = 0; i < len_clothes; i++) {
+    for (let i = 0; i < len_clothes; i++) {
         new_clothes[types.indexOf(clothes[i][1])].push(clothes[i][0])
     }
     
     let answer = 1
-    for (i = 0; i < len_types; i++) {
+    for (let i = 0; i < len_types; i++) {
         answer *= (new_clothes[i].length + 1)
     }
     return answer - 1
