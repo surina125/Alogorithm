f053d29103ba9835f61c312e754aca3f311e05a4
surina125 <jhyunsoo1125@gmail.com>
2024-09-15 16:10:20 +0900

    [level 2] Title: 전화번호 목록, Time: 150.91 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,14 +1,9 @@
 function solution(phone_book) {
     const ph = phone_book.sort()
     for (i = 0; i < ph.length - 1; i++) {
-        num = 0
-        for (j = 0; j < ph[i].length; j++) {
-            if (ph[i][j] === ph[i+1][j]) {
-                num += 1
-            }
-        if (num === ph[i].length) {
+        if (ph[i+1].startsWith(ph[i])) {
             return false
-        }}
+        }
     }
     return true
 }
