1e822fb742c6d60fc670afe228458fb3bfe2f9b4
surina125 <jhyunsoo1125@gmail.com>
2024-09-15 15:58:13 +0900

    [level 2] Title: 전화번호 목록, Time: 108.02 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,10 +1,8 @@
 def solution(phone_book):
-    answer = True
     phone_book.sort()
-    N = len(phone_book)
-    for i in range(N-1):
-        M = len(phone_book[i])
-        if phone_book[i] == phone_book[i+1][:M]:
-            answer = False 
-            break
-    return answer
+    
+    len_ph = len(phone_book)
+    for i in range(len_ph-1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+            return False
+    return True
